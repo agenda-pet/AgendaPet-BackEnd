08d718cd7d1468cc1b8bb4bb81b673a8e965346f
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-06-10 10:30:22 -0300

    :sparkles:feat[structure]: add initial api structure

---
+++ server/AgendaPetAPI/AgendaPetAPI/Domains/PortePet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AgendaPetAPI.Domains;
+
+public partial class PortePet
+{
+    public Guid PorteID { get; set; }
+
+    public string NomePorte { get; set; } = null!;
+
+    public virtual ICollection<Pet> Pet { get; set; } = new List<Pet>();
+}
