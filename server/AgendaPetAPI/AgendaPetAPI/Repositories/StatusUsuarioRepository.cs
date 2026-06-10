b7c9bacf7c63ca2c00719d00ffc9aca7b8ad5779
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-06-10 20:13:29 -0300

    :sparkles:feat[CRUD]: Status usuario adicionado

---
+++ server/AgendaPetAPI/AgendaPetAPI/Repositories/StatusUsuarioRepository.cs
@@ -0,0 +1,22 @@
+﻿using AgendaPetAPI.Contexts;
+using AgendaPetAPI.Domains;
+using AgendaPetAPI.Interfaces;
+
+namespace AgendaPetAPI.Repositories
+{
+    public class StatusUsuarioRepository : IStatusUsuarioRepository
+    {
+        private readonly AgendaPetDbContext _context;
+        public StatusUsuarioRepository(AgendaPetDbContext context) => _context = context;
+
+        public List<StatusUsuario> Listar()
+        {
+            return _context.StatusUsuario.OrderBy(p => p.NomeStatus).ToList();
+        }
+
+        public StatusUsuario ObterPorId(int id)
+        {
+            return _context.StatusUsuario.Find(id);
+        }
+    }
+}
