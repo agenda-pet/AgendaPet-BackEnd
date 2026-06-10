85253cee7f37b4c4e44d7446307db218e67e8c69
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-06-10 20:16:17 -0300

    :sparkles:feat[CRUD]: Tipo animal adicionado

---
+++ server/AgendaPetAPI/AgendaPetAPI/DTOs/TipoAnimalDTO/LerTipoAnimalDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AgendaPetAPI.DTOs.TipoAnimalDTO
+{
+    public class LerTipoAnimalDto
+    {
+        public Guid TipoAnimalID { get; set; }
+
+        public string NomeTipo { get; set; } = null!;
+
+    }
+}
---
+++ server/AgendaPetAPI/AgendaPetAPI/Repositories/TipoAnimalRepostiory.cs
@@ -0,0 +1,22 @@
+﻿using AgendaPetAPI.Contexts;
+using AgendaPetAPI.Domains;
+using AgendaPetAPI.Interfaces;
+
+namespace AgendaPetAPI.Repositories
+{
+    public class TipoAnimalRepostiory : ITipoAnimalRepository
+    {
+        private readonly AgendaPetDbContext _context;
+        public TipoAnimalRepostiory(AgendaPetDbContext context) => _context = context;
+
+        public List<TipoAnimal> Listar()
+        {
+            return _context.TipoAnimal.OrderBy(p => p.NomeTipo).ToList();
+        }
+
+        public TipoAnimal ObterPorId(int id)
+        {
+            return _context.TipoAnimal.Find(id);
+        }
+    }
+}
