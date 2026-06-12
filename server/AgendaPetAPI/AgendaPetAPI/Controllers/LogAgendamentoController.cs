b19b50717ee5ea64a6997d816176d7d58d980851
Pablo Henrique Pedroza da Silva <48558840814@info.local>
2026-06-12 10:57:43 -0300

    CRUD do LogAgendamento feito

---
+++ server/AgendaPetAPI/AgendaPetAPI/Controllers/LogAgendamentoController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AgendaPetAPI.Applications.Service;
+
+namespace AgendaPetAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogAgendamentoController : ControllerBase
+    {
+        private readonly LogAgendamentoService _service;
+
+        public LogAgendamentoController(LogAgendamentoService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            try
+            {
+                return Ok(_service.Listar());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpGet("produto/{id}")]
+        public ActionResult ListarID(Guid id)
+        {
+            try
+            {
+                return Ok(_service.ListarPorID(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+    }
+}
--- server/AgendaPetAPI/AgendaPetAPI/DTOs/LogAgendamentoDTO/LerLogAgendamentoDto.cs
+++ server/AgendaPetAPI/AgendaPetAPI/DTOs/LogAgendamentoDTO/LerLogAgendamentoDto.cs
@@ -1,6 +1,21 @@
-﻿namespace AgendaPetAPI.DTOs.LogAgendamentoDTO
+﻿using AgendaPetAPI.Domains;
+
+namespace AgendaPetAPI.DTOs.LerLogAgendamentoDto
 {
-    public class LerLogAgendamentoDto
+    public class LerAgendamentoDto
     {
+        public Guid LogAgendamentoID { get; set; }
+
+        public DateTime? DataModificacao { get; set; }
+
+        public DateTime DataAnteriorAgendamento { get; set; }
+
+        public string StatusAgendamentoAnterior { get; set; } = null!;
+
+        public string ServicosPorAgendamento { get; set; } = null!;
+
+        public Guid AgendamentoID { get; set; }
+
+        public virtual Agendamento Agendamento { get; set; } = null!;
     }
 }
